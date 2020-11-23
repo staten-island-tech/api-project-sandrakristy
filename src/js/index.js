3ed951cc0324260fd495afae12c7a819e04c6930
lsandra <57500208+lsandra@users.noreply.github.com>
2020-11-22 23:52:05 -0500

    Added stuff but JSON still not working

--- src/js/index.js
+++ src/js/index.js
@@ -1,74 +1,98 @@
+import { genres } from "./genre";
 import{ DOMSelectors} from "./DOM";
 //import "regenerator-runtime/runtime";
 
 
-const query = ` https://jikan.docs.apiary.io/`;
-const query = ` https://jikan.docs.apiary.io/#reference/0/search`;
-const fetch = require('cross-fetch')
-const URL = require('url').URL
+const query = ` https://jikan.moe/`;
 
-module.exports = class Request {
-    constructor(){
-        this.baseURL = "https://api.jikan.moe/v3"
-    }
-    
-    async send(args, params) {
-        var res = await fetch(this.createUrl(args, params))
-        var data = await res.json()
-        if (res.status !== 200) throw `Response: ${res.status}`
-        else return data
-    }
 
-    createUrl (args, params) {
-        const url = new URL(this.baseURL)
-        url.pathname += `/${args.filter(a => a).join("/")}`
-            for (let p in params) {
-                url.searchParams.set(p, params[p])
-            }
-        return url.href
-    }
+const NextPage = async function() {
+    DOMSelectors.nextButton.addEventListener("click", function (e) {
+      DOMSelectors.grid.innerHTML = "";
+      offset += 30;
+      defaultPage();
+    });
+    DOMSelectors.previousButton.addEventListener("click", function (e) {
+      if (offset == 0) {
+        offset = 0;
+      } else {
+        DOMSelectors.grid.innerHTML = "";
+        offset -= 30;
+        defaultPage();
+      }
+    });
+    DOMSelectors.nextButtonB.addEventListener("click", function (e) {
+      DOMSelectors.grid.innerHTML = "";
+      offset += 30;
+      defaultPage();
+    });
+    DOMSelectors.previousButtonB.addEventListener("click", function (e) {
+      if (offset == 0) {
+        offset = 0;
+      } else {
+        DOMSelectors.grid.innerHTML = "";
+        offset -= 30;
+        defaultPage();
+      }
+    });
 }
-const Jikan = require('jikan-node');
-const mal = new Jikan();
 
 const init = async function () {
-  try {
-    const response = await fetch(query);
-    const data = await response.json();
-    console.log(data)
-    const searchBar = document.getElementById("searchBar");
-    console.log(searchBar);
-    searchBar.addEventListener("keyup", function() {
-      console.log("a");
+    try {
+      const response = await fetch(query);
+      const data = await response.json();
+
+    //const searchBar = document.getElementById("searchBar");
+    //console.log(searchBar);
+    //searchBar.addEventListener("keyup", function() {
+     // console.log("a");
   
       //const searchString = e.target.value; //
-    });
-     
-      DOMSelectors.grid.insertAdjacentHTML(
-        "beforeend",
-        `<div class="anime-card">
-      <div class="anime-card-front">
-       
-      </div>
-      <div class="anime-card-back">
-        <h3 class="anime-card-header">${anime.original_title}</h3>
-        <div class="score-box">
-          <p class="user-score">Community Score</p>
-          <p class="user-score">${anime.vote_average}</p>
+      
+    //});
+
+  
+      data.results.forEach((movie) => {
+        let genreArr = [];
+        const addGenre = function () {
+          genres.forEach((element) => {
+            if (movie.genre_ids.includes(element.id)) {
+              genreArr.push(element.name);
+              return genreArr;
+            }
+          });
+        };
+        addGenre();
+        DOMSelectors.grid.insertAdjacentHTML(
+          "beforeend",
+          `<div class="movie-card">
+        <div class="movie-card-front">
+          <img
+            src="https://image.tmdb.org/t/p/w300/${movie.poster_path}"
+            alt=""
+            class="poster"
+          />
         </div>
-        <div class="release-box">
-          <p class="release-date">Released</p>
-          <p class="release-date">${anime.release_date}</p>
+        <div class="movie-card-back">
+          <h3 class="movie-card-header">${movie.original_title}</h3>
+          <div class="score-box">
+            <p class="user-score">Community Score</p>
+            <p class="user-score">${movie.vote_average}</p>
+          </div>
+          <div class="release-box">
+            <p class="release-date">Released</p>
+            <p class="release-date">${movie.release_date}</p>
+          </div>
+          <div class="movie-genres">
+            <div>${genreArr}</div>
+          </div>
         </div>
-        <div class="anime-genres">
-          <div>${genreArr}</div>
-        </div>
-      </div>
-    </div>`
-    );
-    
-  } catch (error) {
-    console.log(error);
-  }
+      </div>`
+        );
+      });
+    } catch (error) {
+      console.log(error);
+    }
 };
-init();
+init();  
+   
