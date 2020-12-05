6bcd7f9dbc558a0c8c09d0faf168dd5869d06391
lsandra <57500208+lsandra@users.noreply.github.com>
2020-12-04 22:20:50 -0500

    Update Dom.js

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,8 +1,9 @@
 const DOMSelectors ={
-    movieGrid: document.querySelector(".movie-grid"),
+    grid: document.querySelector(".anime-grid"),
     btnPrev: document.querySelector(".btn-prev"),
     btnNext: document.querySelector(".btn-next"), 
     animeInput: document.getElementById("searchQuery"),
-};
+   
+}
 
 export{ DOMSelectors };
