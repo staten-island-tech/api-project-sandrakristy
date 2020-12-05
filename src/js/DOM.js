3b7f727e1c29e0e2e3e06d0c4aef5be6beea89ec
lsandra <57500208+lsandra@users.noreply.github.com>
2020-12-05 00:42:00 -0500

    dom

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,9 +1,10 @@
-const DOMSelectors ={
+const DOMSelectors = {
     grid: document.querySelector(".anime-grid"),
+    searchForm: document.getElementById("search-form"),
+    searchArea: document.getElementById("search-area"),
     btnPrev: document.querySelector(".btn-prev"),
     btnNext: document.querySelector(".btn-next"), 
     animeInput: document.getElementById("searchQuery"),
-   
-}
+};
 
 export{ DOMSelectors };
