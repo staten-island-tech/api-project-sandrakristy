8df61521b0e9f52d456fa8b92eb8c886929b9092
lsandra <57500208+lsandra@users.noreply.github.com>
2020-12-05 01:11:03 -0500

    huh

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const DOMSelectors = {
     grid: document.querySelector(".anime-grid"),
     searchForm: document.getElementById("search-form"),
@@ -7,12 +6,5 @@ const DOMSelectors = {
     btnNext: document.querySelector(".btn-next"), 
     animeInput: document.getElementById("searchQuery"),
 };
-=======
-const DOMSelectors ={
-    movieGrid: document.querySelector(".movie-grid"),
-    btnPrev: document.querySelector(".btn-prev"),
-    btnNext: document.querySelector(".btn-next"), 
-}
->>>>>>> bad9564629511eb685fbaaf07614a3f087e9db66
 
-export{ DOMSelectors };
+export {DOMSelectors};
